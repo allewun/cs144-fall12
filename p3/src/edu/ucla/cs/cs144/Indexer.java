bfaae636032539b813d4a524f6ace5f0a8a8b64a
Allen Wu <allen.wu@ucla.edu>
2012-11-01 20:23:10 -0700

    Fixed indentation

--- p3/src/edu/ucla/cs/cs144/Indexer.java
+++ p3/src/edu/ucla/cs/cs144/Indexer.java
@@ -23,39 +23,39 @@ public class Indexer {
         Connection conn = null;
 
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+        try {
+            conn = DbManager.getConnection(true);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
 
 
-	/*
-	 * Add your code here to retrieve Items using the connection
-	 * and add corresponding entries to your Lucene inverted indexes.
+        /*
+         * Add your code here to retrieve Items using the connection
+         * and add corresponding entries to your Lucene inverted indexes.
+             *
+             * You will have to use JDBC API to retrieve MySQL data from Java.
+             * Read our tutorial on JDBC if you do not know how to use JDBC.
+             *
+             * You will also have to use Lucene IndexWriter and Document
+             * classes to create an index and populate it with Items data.
+             * Read our tutorial on Lucene as well if you don't know how.
+             *
+             * As part of this development, you may want to add
+             * new methods and create additional Java classes.
+             * If you create new classes, make sure that
+             * the classes become part of "edu.ucla.cs.cs144" package
+             * and place your class source files at src/edu/ucla/cs/cs144/.
          *
-         * You will have to use JDBC API to retrieve MySQL data from Java.
-         * Read our tutorial on JDBC if you do not know how to use JDBC.
-         *
-         * You will also have to use Lucene IndexWriter and Document
-         * classes to create an index and populate it with Items data.
-         * Read our tutorial on Lucene as well if you don't know how.
-         *
-         * As part of this development, you may want to add
-         * new methods and create additional Java classes.
-         * If you create new classes, make sure that
-         * the classes become part of "edu.ucla.cs.cs144" package
-         * and place your class source files at src/edu/ucla/cs/cs144/.
-	 *
-	 */
+         */
 
 
         // close the database connection
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 
     public static void main(String agrs[]) {
