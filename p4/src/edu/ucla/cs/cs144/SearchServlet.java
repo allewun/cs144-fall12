f029ceb412c20e66c854a773be7d6c9d6b9a56a2
Allen Wu <allen.wu@ucla.edu>
2012-11-13 01:24:40 -0800

    Completed SearchServlet.java

--- p4/src/edu/ucla/cs/cs144/SearchServlet.java
+++ p4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -8,6 +8,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.PrintWriter;
+import javax.servlet.http.HttpUtils;
+import java.util.Hashtable;
+import java.util.Enumeration;
 
 public class SearchServlet extends HttpServlet implements Servlet {
 
@@ -16,12 +19,25 @@ public class SearchServlet extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         PrintWriter out = response.getWriter();
+        AuctionSearchClient as = new AuctionSearchClient();
+
+        String query = request.getParameter("q");
+        int numResultsToSkip = Integer.parseInt(request.getParameter("numResultsToSkip"));
+        int numResultsToReturn = Integer.parseInt(request.getParameter("numResultsToReturn"));
+        SearchResult[] basicResults = as.basicSearch(query, numResultsToSkip, numResultsToReturn);
+
         out.println("<html>");
-        out.println("<head><title>Servlet Example</title></head>");
+        out.println("<head><title>eBay Keyword Search Results</title></head>");
         out.println("<body>");
-        out.println(request);
-        out.println("<hr>");
-        out.println(response);
+
+        out.println("<h1>Basic Search Query: " + query + "</h1>");
+        out.println("<h2>Received " + basicResults.length + " results</h2>");
+        out.println("<ol>");
+        for (SearchResult result : basicResults) {
+            out.println("<li>" + result.getItemId() + ": " + result.getName() + "</li>");
+        }
+        out.println("</ol>");
+
         out.println("</body>");
         out.println("</html>");
         out.close();
