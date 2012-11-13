94fc2dcf8717a8956d28c7d8e1aa90c576982295
Allen Wu <allen.wu@ucla.edu>
2012-11-12 23:59:28 -0800

    Added keywordSearch.html and beginning work on the search servlet

--- p4/src/edu/ucla/cs/cs144/SearchServlet.java
+++ p4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -7,12 +7,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.PrintWriter;
+
 public class SearchServlet extends HttpServlet implements Servlet {
 
     public SearchServlet() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>Servlet Example</title></head>");
+        out.println("<body>");
+        out.println(request);
+        out.println("<hr>");
+        out.println(response);
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
     }
 }
