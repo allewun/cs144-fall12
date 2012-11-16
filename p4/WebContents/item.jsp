e91b9ee43ea7825091edd4a0cfee3006bcc564a4
Samuel Jun <samuel.h.jun@gmail.com>
2012-11-16 01:57:15 -0800

    Implemented most of ItemServlet. Categories, Buy_Price, and Bids still not implemented

--- p4/WebContents/item.jsp
+++ p4/WebContents/item.jsp
@@ -5,8 +5,19 @@
 	</head>
 	<body>
 		<h1>Item Results</h1>
-		ItemID: <%= request.getParameter("id") %> <br />
-        ItemName: <%= request.getAttribute("itemName") %> <br />
-        ItemDescription: <%= request.getAttribute("itemDescription") %> <br />
+		ID: <%= request.getParameter("id") %> <br />
+        Name: <%= request.getAttribute("itemName") %> <br />
+        Category: <br />
+        Currently: <%= request.getAttribute("itemCurrently") %> <br />
+        First_Bid: <%= request.getAttribute("itemFirstBid") %> <br />
+        Number_of_Bids: <%= request.getAttribute("itemNumberOfBids") %> <br />
+        Bids: <br />
+        Location: <%= request.getAttribute("itemLocation") %> <br />
+        Country: <%= request.getAttribute("itemCountry") %> <br />
+        Started: <%= request.getAttribute("itemStarted") %> <br />
+        Ends: <%= request.getAttribute("itemEnds") %> <br />
+        Seller UserID: <%= request.getAttribute("itemSellerUserID") %> <br />
+        Seller Rating: <%= request.getAttribute("itemSellerRating") %> <br />
+        Description: <%= request.getAttribute("itemDescription") %> <br />
 	</body>
 </html>
--- p4/src/edu/ucla/cs/cs144/ItemServlet.java
+++ p4/src/edu/ucla/cs/cs144/ItemServlet.java
@@ -19,6 +19,10 @@ import javax.xml.xpath.XPathExpressionException;
 import org.xml.sax.SAXException;
 import java.io.StringReader;
 import org.xml.sax.InputSource;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import javax.xml.xpath.XPathConstants;
+import java.util.Vector;
 
 public class ItemServlet extends HttpServlet implements Servlet {
 
@@ -27,8 +31,6 @@ public class ItemServlet extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         try {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/item.jsp");
-
             AuctionSearchClient as = new AuctionSearchClient();
 
             String itemId = request.getParameter("id");
@@ -39,17 +41,76 @@ public class ItemServlet extends HttpServlet implements Servlet {
             Document doc = builder.parse(new InputSource(new StringReader(itemXML)));
             XPathFactory xPathfactory = XPathFactory.newInstance();
             XPath xpath = xPathfactory.newXPath();
-            XPathExpression expr = xpath.compile("Item/Name");
+            XPathExpression expr = null;
 
+            // Item/Name
+            expr = xpath.compile("Item/Name");
             String itemName = expr.evaluate(doc);
+            request.setAttribute("itemName", itemName);
+            // Item/Category
+            expr = xpath.compile("Item/Category");
+            NodeList itemCategoryNodes = (NodeList)expr.evaluate(doc, XPathConstants.NODESET);
+            Vector itemCategoriesVector = new Vector();
+            for (int i = 0; i < itemCategoryNodes.getLength(); i++) {
+                Node currentNode = itemCategoryNodes.item(i);
+                itemCategoriesVector.add(currentNode.getTextContent());
+            }
+            String[] itemCategories = new String[itemCategoriesVector.size()];
+            itemCategoriesVector.toArray(itemCategories);
+            //request.setAttribute("itemCategories", itemCategories);
+            // Item/Currently
+            expr = xpath.compile("Item/Currently");
+            String itemCurrently = expr.evaluate(doc);
+            request.setAttribute("itemCurrently", itemCurrently);
+            // Item/Buy_Price 
+            if (itemXML.contains("<Buy_Price>"))
+            {
+                expr = xpath.compile("Item/Buy_Price");
+                String itemBuyPrice = expr.evaluate(doc);
+                request.setAttribute("itemBuyPrice", itemBuyPrice);
+            }
+            // Item/First_Bid
+            expr = xpath.compile("Item/First_Bid");
+            String itemFirstBid = expr.evaluate(doc);
+            request.setAttribute("itemFirstBid", itemFirstBid);
+            // Item/Number_of_Bids
+            expr = xpath.compile("Item/Number_of_Bids");
+            String itemNumberOfBids = expr.evaluate(doc);
+            request.setAttribute("itemNumberOfBids", itemNumberOfBids);
+            // Item/Bids
+            //
+            // Item/Location
+            expr = xpath.compile("Item/Location");
+            String itemLocation = expr.evaluate(doc);
+            request.setAttribute("itemLocation", itemLocation);
+            // Item/Country
+            expr = xpath.compile("Item/Country");
+            String itemCountry = expr.evaluate(doc);
+            request.setAttribute("itemCountry", itemCountry);
+            // Item/Started
+            expr = xpath.compile("Item/Started");
+            String itemStarted = expr.evaluate(doc);
+            request.setAttribute("itemStarted", itemStarted);
+            // Item/Ends
+            expr = xpath.compile("Item/Ends");
+            String itemEnds = expr.evaluate(doc);
+            request.setAttribute("itemEnds", itemEnds);
+            // Item/Seller@UserID
+            expr = xpath.compile("Item/Seller/@UserID");
+            String itemSellerUserID = expr.evaluate(doc);
+            request.setAttribute("itemSellerUserID", itemSellerUserID);
+            // Item/Seller@Rating
+            expr = xpath.compile("Item/Seller/@Rating");
+            String itemSellerRating = expr.evaluate(doc);
+            request.setAttribute("itemSellerRating", itemSellerRating);
 
+            // Item/Description
             expr = xpath.compile("Item/Description");
             String itemDescription = expr.evaluate(doc);
-
-            request.setAttribute("itemName", itemName);
             request.setAttribute("itemDescription", itemDescription);
-            rd.forward(request, response);
-            //request.getRequestDispatcher("/item.jsp").forward(request, response);
+
+
+            request.getRequestDispatcher("/item.jsp").forward(request, response);
         }
         catch (ServletException e) {
             // TODO Auto-generated catch block
