8ce17e322fae66a573a2794e546e258871915085
Allen Wu <allen.wu@ucla.edu>
2012-11-26 19:31:11 -0800

    Validating item ID

---
+++ p4/WebContents/js/ebay.js
@@ -0,0 +1,33 @@
+
+String.prototype.trim = function () {
+	return this.replace(/^\s+|\s+$/g, "");
+};
+
+// adapted from http://ejohn.org/projects/flexible-javascript-events/
+function addEvent(id, type, fn) {
+	var el = document.getElementById(id);
+	if (el.addEventListener) {
+		el.addEventListener(type, fn, false);
+	}
+	else if (el.attachEvent) {
+		el["e"+type+fn] = fn;
+		el[type+fn] = function() { el["e"+type+fn](window.event); };
+		el.attachEvent("on" + type, el[type+fn]);
+	}
+}
+
+function preventDefault(evt) {
+	evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+}
+
+function validateItemId(evt) {
+	var value = document.getElementById("itemId").value;
+	if (value.trim().search(/\D/g) != -1) {
+		alert("The Item ID must be a positive integer.");
+		preventDefault(evt);
+	}
+	else {
+		document.getElementById("itemId").value = value.trim();
+		return true;
+	}
+}
